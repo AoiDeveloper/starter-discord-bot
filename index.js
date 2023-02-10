564a3cc7632a84ef2a4f19e658d20ce1586791cc
AoiDeveloper <76095792+AoiDeveloper@users.noreply.github.com>
2023-02-10 17:17:51 +0900

    fix

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ client.on(Events.MessageCreate, (message) => {
 
       // Get a random line
       const randomLine = lines[Math.floor(Math.random() * lines.length)].trim();
-      message.channel.send(message.member.username + "あなたが使うブキは" + randomLine + "です。");
+      message.channel.send(message.member.displayName + "あなたが使うブキは" + randomLine + "です。");
     } else {
       const users = member.voice.channel.members;
       for (const user of users) {
