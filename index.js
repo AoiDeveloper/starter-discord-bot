da9a8b9fea7a2000b3a08be8d37b976393e99daf
AoiDeveloper <76095792+AoiDeveloper@users.noreply.github.com>
2023-02-10 17:05:34 +0900

    init

--- index.js
+++ index.js
@@ -1,113 +1,40 @@
+const dotenv = require("dotenv").config();
+// Load the fs module
+const fs = require('fs');
 
-// const { clientId, guildId, token, publicKey } = require('./config.json');
-require('dotenv').config()
-const APPLICATION_ID = process.env.APPLICATION_ID 
-const TOKEN = process.env.TOKEN 
-const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
+const {
+  Client,
+  Events,
+  GatewayIntentBits: { Guilds, GuildMessages, MessageContent },
+} = require("discord.js");
+const options = {
+  intents: [Guilds, GuildMessages, MessageContent],
+};
 
+const client = new Client(options);
+client.login(process.env.BOT_TOKEN);
 
-const axios = require('axios')
-const express = require('express');
-const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
+console.log("ready");
+client.on(Events.MessageCreate, (message) => {
+  console.log("message on.")
+  const sender = message.author;
+  if (message.content == "!random_buki") {
+    if (!sender.voice) {
+      // Read the file
+      const csvFile = fs.readFileSync('buki.csv', 'utf8');
 
+      // Split the csv content by line
+      const lines = csvFile.split('\n');
 
-const app = express();
-// app.use(bodyParser.json());
-
-const discord_api = axios.create({
-  baseURL: 'https://discord.com/api/',
-  timeout: 3000,
-  headers: {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-	"Access-Control-Allow-Headers": "Authorization",
-	"Authorization": `Bot ${TOKEN}`
-  }
-});
-
-
-
-
-app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
-  const interaction = req.body;
-
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    console.log(interaction.data.name)
-    if(interaction.data.name == 'yo'){
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `Yo ${interaction.member.user.username}!`,
-        },
-      });
-    }
-
-    if(interaction.data.name == 'dm'){
-      // https://discord.com/developers/docs/resources/user#create-dm
-      let c = (await discord_api.post(`/users/@me/channels`,{
-        recipient_id: interaction.member.user.id
-      })).data
-      try{
-        // https://discord.com/developers/docs/resources/channel#create-message
-        let res = await discord_api.post(`/channels/${c.id}/messages`,{
-          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
-        })
-        console.log(res.data)
-      }catch(e){
-        console.log(e)
+      // Get a random line
+      const randomLine = lines[Math.floor(Math.random() * lines.length)].trim();
+      message.channel.send(sender.name + "あなたが使うブキは" + randomLine + "です。");
+    } else {
+      const users = member.voice.channel.members;
+      for (const user of users) {
+        const randomLine = lines[Math.floor(Math.random() * lines.length)].trim();
+        message.channel.send(user.name + "あなたが使うブキは" + randomLine + "です。");
       }
-
-      return res.send({
-        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data:{
-          content:'👍'
-        }
-      });
     }
   }
-
 });
-
-
-
-app.get('/register_commands', async (req,res) =>{
-  let slash_commands = [
-    {
-      "name": "yo",
-      "description": "replies with Yo!",
-      "options": []
-    },
-    {
-      "name": "dm",
-      "description": "sends user a DM",
-      "options": []
-    }
-  ]
-  try
-  {
-    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
-    let discord_response = await discord_api.put(
-      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
-      slash_commands
-    )
-    console.log(discord_response.data)
-    return res.send('commands have been registered')
-  }catch(e){
-    console.error(e.code)
-    console.error(e.response?.data)
-    return res.send(`${e.code} error from discord`)
-  }
-})
-
-
-app.get('/', async (req,res) =>{
-  return res.send('Follow documentation ')
-})
-
-
-app.listen(8999, () => {
-
-})
-
